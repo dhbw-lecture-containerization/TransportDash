5c0b8b5b038e9a31125dabe9bd79c6bbf4a649c2
Diluvenix <jonas.putz@icloud.com>
2026-03-15 15:55:43 +0100

    Adds basic warning insertion

--- dags/car_traffic_dag.py
+++ dags/car_traffic_dag.py
@@ -3,7 +3,7 @@ import json
 import pendulum
 
 import requests
-from airflow.sdk import dag, task
+from airflow.sdk import dag, task, get_current_context
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
@@ -38,27 +38,62 @@ def CarTrafficDag():
         conn_id="postgres_conn",
         sql="""
             CREATE TABLE IF NOT EXISTS car.highways (
-                id INTEGER PRIMARY KEY,
+                id SERIAL PRIMARY KEY,
                 name VARCHAR(10) NOT NULL
             );""",
     )
 
+    create_warnings_table = SQLExecuteQueryOperator(
+        task_id="create_warnings_table",
+        conn_id="postgres_conn",
+        sql="""
+            CREATE TABLE IF NOT EXISTS car.warnings (
+                id char(64) PRIMARY KEY,
+                highwayId INTEGER NOT NULL REFERENCES car.highways(id) ON DELETE CASCADE
+            );""",
+    )
+
+    create_warning_timestamps_table = SQLExecuteQueryOperator(
+        task_id="create_warning_timestamps_table",
+        conn_id="postgres_conn",
+        sql="""
+            CREATE TABLE IF NOT EXISTS car.warningTimestamps (
+                warningId char(64) NOT NULL REFERENCES car.warnings(id) ON DELETE CASCADE,
+                timestampId INTEGER NOT NULL REFERENCES car.timestamps(id) ON DELETE CASCADE,
+                PRIMARY KEY (warningId, timestampId)
+            );"""
+    )
+
     @task
     def get_highways():
+        sql = """
+            WITH ret AS (
+                INSERT INTO car.highways (name) VALUES ('{name}') 
+                ON CONFLICT DO NOTHING
+                RETURNING id
+            ) 
+            SELECT * FROM ret
+            UNION
+                SELECT id FROM car.highways WHERE name='{name}';
+        """
+
         resp = requests.get("https://verkehr.autobahn.de/o/autobahn/")
-        if (not resp.ok): return 1
+        if (not resp.ok): raise resp.raise_for_status()
 
         data = json.loads(resp.content)
+        roads = []
 
         postgres_hook = PostgresHook(postgres_conn_id="postgres_conn")
         conn = postgres_hook.get_conn()
         cur = conn.cursor()
 
-        for idx, road in enumerate(data["roads"]):
-            print(f"{idx}: \"{road}\"")
-            cur.execute(f"INSERT INTO car.highways (id, name) VALUES ({idx}, '{road}') ON CONFLICT DO NOTHING")
-
+        for road in data["roads"]:
+            cur.execute(sql.format(name=road))
+            roads.append((cur.fetchone()[0], road))
         conn.commit()
+
+        return roads
+    get_highways = get_highways()
 
     @task
     def create_timestamp():
@@ -70,13 +105,51 @@ def CarTrafficDag():
         conn.commit()
 
         return cur.fetchone()[0]
+    create_timestamp = create_timestamp()
+    
+    @task
+    def get_warnings():
+        sql1 = """
+            INSERT INTO car.warnings (id, highwayId) 
+                VALUES ('{id}', {highway_id})
+                ON CONFLICT DO NOTHING;
+        """
+        sql2 = """
+            INSERT INTO car.warningTimestamps (warningId, timestampId)
+                VALUES ('{warning_id}', {timestamp_id});
+        """
+        task_instance = get_current_context()["ti"]
+        timestamp_id = task_instance.xcom_pull(task_ids='create_timestamp')
+        highways = task_instance.xcom_pull(task_ids='get_highways')
+
+        postgres_hook = PostgresHook(postgres_conn_id="postgres_conn")
+        conn = postgres_hook.get_conn()
+        cur = conn.cursor()
+
+        for (highway_id, highway_name) in highways:
+            resp = requests.get(f"https://verkehr.autobahn.de/o/autobahn/{highway_name}/services/warning")
+            if (not resp.ok): raise resp.raise_for_status()
+
+            data = json.loads(resp.content)
+            warnings = data["warning"]
+
+            for warning in warnings:
+                cur.execute(sql1.format(id=warning["identifier"], highway_id=highway_id))
+                cur.execute(sql2.format(warning_id=warning["identifier"], timestamp_id=timestamp_id))
+                break
+            break
+        
+        conn.commit()
+    get_warnings = get_warnings()
 
     create_schema >> create_timestamp_table
     create_schema >> create_highway_table
+    [create_schema, create_highway_table] >> create_warnings_table
+    [create_schema, create_timestamp_table, create_warnings_table] >> create_warning_timestamps_table
 
-    [create_timestamp_table, create_highway_table] >> get_highways()
-    [create_timestamp_table, create_highway_table] >> create_timestamp()
+    [create_timestamp_table, create_highway_table] >> get_highways
+    [create_timestamp_table, create_highway_table] >> create_timestamp
 
-    # create_schema >> [create_timestamp_table, create_highway_table] >> [get_highways(), create_timestamp()]
+    [create_warnings_table, create_warning_timestamps_table, get_highways, create_timestamp] >> get_warnings
 
 dag = CarTrafficDag()
