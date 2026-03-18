193a4d6361a5be263e650f7a9b19cfc093b6922f
Diluvenix <146968909+Diluvenix@users.noreply.github.com>
2026-03-18 17:30:14 +0100

    12 car traffic dashboard (#15)
    * Adds basic map
    
    * adds a simple overview bar
    
    * adds roadworks
    
    * fixes dag
    
    * adds roadworks to graph
    
    * changes the dag to run every 10 minutes
    
    * Adds dataframe to streamlit view

--- dags/car_traffic_dag.py
+++ dags/car_traffic_dag.py
@@ -1,6 +1,7 @@
 import datetime
 import json
 import pendulum
+from tqdm import tqdm
 
 import requests
 from airflow.sdk import dag, task, get_current_context
@@ -16,17 +17,11 @@ def parse_warning(warning : dict):
         "longitude": float(warning["coordinate"]["long"]),
     }
 
-    a, b, c, d = warning["extent"].split(",")
-    out["bboxLat1"] = float(a)
-    out["bboxLong1"] = float(b)
-    out["bboxLat2"] = float(c)
-    out["bboxLong2"] = float(d)
-
     return out
 
 @dag(
     dag_id="car_traffic_dag",
-    schedule="0 0 * * *",
+    schedule="*/10 * * * *",
     start_date=pendulum.datetime(2026, 3, 12),
     catchup=False,
     dagrun_timeout=datetime.timedelta(minutes=60)
@@ -65,16 +60,13 @@ def CarTrafficDag():
         conn_id="postgres_conn",
         sql="""
             CREATE TABLE IF NOT EXISTS car.warnings (
-                id char(64) PRIMARY KEY,
+                id char(128) PRIMARY KEY,
                 highwayId INTEGER NOT NULL REFERENCES car.highways(id) ON DELETE CASCADE,
                 title TEXT NOT NULL,
                 description TEXT NOT NULL,
+                type TEXT NOT NULL,
                 latitude DOUBLE PRECISION NOT NULL,
-                longitude DOUBLE PRECISION NOT NULL,
-                bboxLatitude1 DOUBLE PRECISION NOT NULL,
-                bboxLongitude1 DOUBLE PRECISION NOT NULL,
-                bboxLatitude2 DOUBLE PRECISION NOT NULL,
-                bboxLongitude2 DOUBLE PRECISION NOT NULL
+                longitude DOUBLE PRECISION NOT NULL
             );""",
     )
 
@@ -83,7 +75,7 @@ def CarTrafficDag():
         conn_id="postgres_conn",
         sql="""
             CREATE TABLE IF NOT EXISTS car.warningTimestamps (
-                warningId char(64) NOT NULL REFERENCES car.warnings(id) ON DELETE CASCADE,
+                warningId char(128) NOT NULL REFERENCES car.warnings(id) ON DELETE CASCADE,
                 timestampId INTEGER NOT NULL REFERENCES car.timestamps(id) ON DELETE CASCADE,
                 PRIMARY KEY (warningId, timestampId)
             );"""
@@ -135,8 +127,8 @@ def CarTrafficDag():
     @task
     def get_warnings():
         sql = """
-            INSERT INTO car.warnings (id, highwayId, title, description, latitude, longitude, bboxLatitude1, bboxLongitude1, bboxLatitude2, bboxLongitude2) 
-                VALUES ({id!r}, {highway_id}, {title!r}, {description!r}, {latitude}, {longitude}, {bboxLat1}, {bboxLong1}, {bboxLat2}, {bboxLong2})
+            INSERT INTO car.warnings (id, highwayId, title, description, type, latitude, longitude) 
+                VALUES ({id!r}, {highway_id}, {title!r}, {description!r}, {type!r}, {latitude}, {longitude})
                 ON CONFLICT DO NOTHING;
         """
         task_instance = get_current_context()["ti"]
@@ -147,7 +139,7 @@ def CarTrafficDag():
         conn = postgres_hook.get_conn()
         cur = conn.cursor()
 
-        for (highway_id, highway_name) in highways:
+        for (highway_id, highway_name) in tqdm(highways):
             resp = requests.get(f"https://verkehr.autobahn.de/o/autobahn/{highway_name}/services/warning")
             if (not resp.ok): raise resp.raise_for_status()
 
@@ -156,9 +148,20 @@ def CarTrafficDag():
 
             for warning in warnings:
                 warning = parse_warning(warning)
-                cur.execute(sql.format(highway_id=highway_id, **warning))
+                cur.execute(sql.format(highway_id=highway_id, type="Warnung", **warning))
                 warning_ids.append(warning["id"])
-        
+            
+            resp = requests.get(f"https://verkehr.autobahn.de/o/autobahn/{highway_name}/services/roadworks")
+            if (not resp.ok): raise resp.raise_for_status()
+
+            data = json.loads(resp.content)
+            warnings = data["roadworks"]
+
+            for warning in warnings:
+                warning = parse_warning(warning)
+                cur.execute(sql.format(highway_id=highway_id, type="Baustelle", **warning))
+                warning_ids.append(warning["id"])
+
         conn.commit()
         return warning_ids
     get_warnings = get_warnings()
--- streamlit/pages/2_Car_Traffic.py
+++ streamlit/pages/2_Car_Traffic.py
@@ -1,9 +1,92 @@
+from zoneinfo import ZoneInfo
 import streamlit as st
+import pydeck as pdk
+import pandas as pd
+import psycopg2
+import textwrap
+
+db_connection = psycopg2.connect(
+    host="db",
+    user="postgres",
+    password="postgres",
+    database="postgres",
+    port=5432
+)
+cursor = db_connection.cursor()
+
+cursor.execute("SELECT * FROM car.timestamps ORDER BY timestamp DESC LIMIT 1")
+(recent_timestamp_id, recent_timestamp) = cursor.fetchall()[0]
+
+recent_warnings = []
+cursor.execute(f"""
+    SELECT * 
+        FROM car.warningtimestamps AS wt 
+        LEFT JOIN car.warnings AS w ON wt.warningId=w.id 
+        LEFT JOIN car.highways AS h ON h.id=w.highwayId 
+        LEFT JOIN car.timestamps AS t ON t.id=wt.timestampId
+    WHERE wt.timestampid={recent_timestamp_id}
+""")
+def extract_warning(warning):
+    (id, _, _, _, title, description, type, lat, long, _, highway, _, timestamp) = warning
+    return {
+        "id": id,
+        "title": title,
+        "description": "\n".join(textwrap.wrap(description.replace("\\n", "\n"), width=80, replace_whitespace=False)),
+        "lat": lat,
+        "lon": long,
+        "highway": highway,
+        "timestamp": timestamp,
+        "type": type
+    }
+for warning in cursor.fetchall():
+    recent_warnings.append(extract_warning(warning))
+recent_warnings = pd.DataFrame(recent_warnings)
+
+def get_color(row):
+    if row["type"] == "Warnung":
+        return [255, 0, 0]
+    elif row["type"] == "Baustelle":
+        return [255, 165, 0]
+    else:
+        return [0, 255, 0]
+recent_warnings["color"] = recent_warnings.apply(get_color, axis=1)
+def get_radius(row):
+    if row["type"] == "Warnung":
+        return 5000
+    elif row["type"] == "Baustelle":
+        return 1000
+    else:
+        return 5000
+recent_warnings["radius"] = recent_warnings.apply(get_radius, axis=1)
 
 st.title("🚗 Car Traffic")
 
-st.write("Car traffic data overview")
+col1, col2, col3 = st.columns(3)
+col1.metric("🚧 Baustellen", sum(recent_warnings["type"]=="Baustelle"))
+col2.metric("⚠️ Warnungen", sum(recent_warnings["type"]=="Warnung"))
+col3.metric("⏱️ Aktualisiert", recent_timestamp.astimezone(ZoneInfo("Europe/Berlin")).strftime("%d.%m (%H:%M)"))
 
-# Example placeholder
-st.metric("Cars today", 54000)
-st.bar_chart([100, 200, 150, 300])
+st.pydeck_chart(pdk.Deck(
+    initial_view_state=pdk.ViewState(
+        latitude=recent_warnings["lat"].mean(),
+        longitude=recent_warnings["lon"].mean(),
+        zoom=6,
+    ),
+    layers=[
+        pdk.Layer(
+            "ScatterplotLayer",
+            data=recent_warnings,
+            get_position='[lon, lat]',
+            get_color='color',
+            get_radius='radius',
+            pickable=True,
+        ),
+    ],
+    tooltip={"text": "{highway}: {type}\n{description}"}
+))
+
+st.subheader("Alle Events als Tabelle")
+st.dataframe(recent_warnings.drop(columns=["id", "color", "radius"]))
+
+cursor.close()
+db_connection.close()
